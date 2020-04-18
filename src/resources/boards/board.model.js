69ed8baa7d8d73c8ee56a28d42ccca85194ea63b
NatalieButr <natashabutrim@gmail.com>
2020-04-18 18:59:22 +0300

    create boardSchema for mongo

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,15 +1,32 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
+
 const Column = require('../columns/column.model');
 
-class Board {
-  constructor({ id = uuid(), title, columns } = { title, columns }) {
-    this.id = id;
-    this.title = title;
-    this.columns =
-      columns !== undefined && columns.length
-        ? columns.map(item => new Column(item))
-        : null;
-  }
-}
+const boardSchema = new mongoose.Schema(
+  {
+    title: String,
+    columns: Array,
+    password: String,
+    _id: {
+      type: String,
+      default: uuid
+    }
+  },
+  { versionKey: false }
+);
+
+const Board = mongoose.model('Board', boardSchema);
+
+// class Board {
+//   constructor({ id = uuid(), title, columns } = { title, columns }) {
+//     this.id = id;
+//     this.title = title;
+//     this.columns =
+//       columns !== undefined && columns.length
+//         ? columns.map(item => new Column(item))
+//         : null;
+//   }
+// }
 
 module.exports = Board;
