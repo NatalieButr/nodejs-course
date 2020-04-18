703201fd1ea70b06bb30c06459a57d3d2943c06e
NatalieButr <natashabutrim@gmail.com>
2020-04-18 19:12:00 +0300

    create data in mongo db

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -2,11 +2,35 @@ const mongoose = require('mongoose');
 const { MONGO_CONNECTION_STRING } = require('../common/config');
 
 const User = require('../resources/users/user.model');
-// data
+const Board = require('../resources/boards/board.model');
+const Task = require('../resources/tasks/task.model');
 
+// default data
 const users = [
   new User({ name: 'Harry', login: 'potter', password: 'tomredl' }),
   new User({ name: 'Hermiona', login: 'hermiona', password: 'sksk' })
+];
+const boards = [
+  new Board({ title: 'First Board', columns: [] }),
+  new Board({ title: 'Second Board', columns: [] })
+];
+const tasks = [
+  new Task({
+    title: 'Task 1',
+    order: 1,
+    description: 'Hello World',
+    userId: '13',
+    boardId: '24',
+    columnId: '32'
+  }),
+  new Task({
+    title: 'Task 21',
+    order: 1,
+    description: '2 World',
+    userId: '13',
+    boardId: '24',
+    columnId: '32'
+  })
 ];
 
 const connectToMongo = callback => {
@@ -22,6 +46,8 @@ const connectToMongo = callback => {
     console.log('mongoose connected');
     db.dropDatabase();
     users.forEach(user => user.save());
+    boards.forEach(board => board.save());
+    tasks.forEach(task => task.save());
     callback();
   });
 };
